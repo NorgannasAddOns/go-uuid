93a28ef83484849172b3315e46cf4010c74f1b3c
Kenneth Allan <ken@norganna.com>
2015-03-25 23:10:27 +1000

    Updates to id's precision

--- uuid.go
+++ uuid.go
@@ -2,16 +2,17 @@ package uuid
 
 import (
 	"time"
+	"math"
 	"math/rand"
 )
 
 var safeChars string = "23456789ABCDEFGHJKLMNPQRSTWXYZabcdefghijkmnopqrstuvwxyz"
-var safeCharsIdx map[rune]int64 = map[rune]int64{}
-var timeFrame int64 = 86400000 * 7
+var safeCharsIdx map[byte]float64 = map[byte]float64{}
+var timeFrame float64 = 86400000 * 7
 
 func init() {
 	for i, c := range safeChars {
-		safeCharsIdx[c] = int64(i)
+		safeCharsIdx[byte(c)] = float64(i)
 	}
 }
 
@@ -40,18 +41,30 @@ func Valid(what string) bool {
 }
 
 func Date(what string) *time.Time {
-	if len(what) != 20 {
+	if len(what) != 20 || !Valid(what) {
 		return nil
 	}
 
-	a := safeCharsIdx[rune(what[1])]
-	b := safeCharsIdx[rune(what[2])]
-	c := safeCharsIdx[rune(what[3])]
+	a := safeCharsIdx[byte(what[0])]
+	b := safeCharsIdx[byte(what[1])]
+	c := safeCharsIdx[byte(what[2])]
+	d := safeCharsIdx[byte(what[3])]
+	e := safeCharsIdx[byte(what[4])]
 
-	var t int64
-	t = (a * 55 + b + 2000) * timeFrame + ^^(c * timeFrame / 55)
-	tm := time.Unix(^^(t/1000), (t % 1000) * 1000)
+	t := (a * 55 + b + 2000) * timeFrame
+	t += math.Floor(c * timeFrame / 55)
+	t += math.Floor(d * timeFrame / (55 * 55))
+	t += math.Floor(e * timeFrame / (55 * 55 * 55))
+	tm := time.Unix(int64(math.Floor(t/1000)), (int64(t) % 1000) * 1000000)
 	return &tm
+}
+
+func Type(what string) string {
+	if len(what) != 20 || !Valid(what) {
+		return ""
+	}
+
+	return string(what[5])
 }
 
 func New(c string) string {
@@ -60,18 +73,30 @@ func New(c string) string {
 	}
 
  	d := make([]byte, 20)
-	d[0] = c[0]
-	now := ^^(time.Now().UnixNano() / 1000000)
-	weeks := ^^(now / timeFrame)
-	remain := int64((float64(now - (weeks * timeFrame)) / float64(timeFrame)) * 55)
+	var now, weeks, remain, remain2, remain3, offset, scale float64
+
+	now  = float64(time.Now().UnixNano())/1000000
+	weeks = math.Floor(now / timeFrame)
+	offset = now - weeks * timeFrame
+	scale = 55
+	remain = math.Floor(offset / timeFrame * scale)
+	offset -= remain * timeFrame / scale
+	scale *= 55
+	remain2 = math.Floor(offset / timeFrame * scale)
+	offset -= remain2 * timeFrame / scale
+	scale *= 55
+	remain3 = math.Floor(offset / timeFrame * scale)
 
 	weeks -= 2000;
 
-	d[1] = safeChars[^^(weeks / 55) % 55]
-	d[2] = safeChars[weeks % 55]
-	d[3] = safeChars[remain]
+	d[0] = safeChars[int64(math.Floor(weeks / 55)) % 55]
+	d[1] = safeChars[int64(weeks) % 55]
+	d[2] = safeChars[int64(remain)]
+	d[3] = safeChars[int64(remain2)]
+	d[4] = safeChars[int64(remain3)]
+	d[5] = c[0]
 
-	for i := 4; i < 19; i++ {
+	for i := 6; i < 19; i++ {
 		d[i] = safeChars[rand.Int31n(54)]
 	}
 	d[19] = 0;
--- uuid_test.go
+++ uuid_test.go
@@ -2,14 +2,57 @@ package uuid
 
 import (
 	"fmt"
+	"time"
 	"testing"
 )
 
 func TestUUID(t *testing.T) {
-	uuid := New("T")
-	fmt.Println("UUID", uuid)
-	fmt.Println("Date", Date(uuid))
-	fmt.Println("Valid", Valid(uuid))
+	id := New("T")
+	fmt.Println("New UUID", id)
 
-	fmt.Println("Date", Date("L8ZEmwuQu2WoXJQhZuz7"))
+	if !Valid(id) {
+		fmt.Println("Newly generated id is not valid")
+		t.FailNow()
+	}
+	fmt.Println(" - is valid")
+
+	if Type(id) != "T" {
+		fmt.Println("Newly generated id is not of right type")
+		t.FailNow()
+	}
+	fmt.Println(" - is correct type");
+
+	age := time.Now().Sub(*Date(id))
+	if age.Seconds() > 5 {
+		fmt.Println("Newly generated id is too old", age);
+		t.FailNow()
+	}
+	if age.Seconds() < 0 {
+		fmt.Println("Newly generated id is too young");
+		t.FailNow()
+	}
+	fmt.Println(" - is only", age, "old");
+
+	id = "8ZwLeTmHZDb89WknvTtq"
+	fmt.Println("Constant UUID", id)
+
+	if !Valid(id) {
+		fmt.Println("Constant id is not valid")
+		t.FailNow()
+	}
+	fmt.Println(" - is valid")
+
+	if Type(id) != "T" {
+		fmt.Println("Constant id is not of right type")
+		t.FailNow()
+	}
+	fmt.Println(" - is correct type");
+
+	date := Date(id).UTC().Format(time.RFC3339)
+
+	if date != "2015-03-25T12:48:56Z" {
+		fmt.Println("Constant id did not decode correct date")
+		t.FailNow()
+	}
+	fmt.Println(" - date is correct", date)
 }
